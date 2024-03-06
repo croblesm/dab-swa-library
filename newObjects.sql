9ef777116c38406714c840983c84ea58494ff649
Carlos Robles <contact@croblesm.com>
2024-03-06 00:44:16 +0000

    Updating script name:

--- newObjects.sql
+++
@@ -1,7 +0,0 @@
--- Create series table
-CREATE TABLE dbo.series (
-    id          INT IDENTITY (1, 1) NOT NULL,
-    name        NVARCHAR (255) NOT NULL,
-    description NVARCHAR(MAX) NULL
-);
-GO
